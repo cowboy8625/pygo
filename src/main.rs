604a7d57cf2d1e71ebd4b9c84cd4d645bb6701d9
cowboy8625 <cowboy8625@yahoo.com>
2021-05-18 19:12:15 -0600

    Formated code

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clap::{crate_name, crate_version, crate_description, App, Arg, SubCommand};
+use clap::{crate_description, crate_name, crate_version, App, Arg, SubCommand};
 use std::fs::{create_dir, File};
 use std::io::prelude::*;
 const AUTHOR: &str = "
@@ -35,24 +35,30 @@ fn create_basic_dir(name: &str) {
     create_dir(format!("{}/src", name)).expect("Could not create src Folder.");
     std::process::Command::new("git")
         .args(&["init", name])
-        .spawn().expect("Creating a git repository Failed.");
-    let mut file = File::create(format!("{}/README.md", name)).expect("Could not create __main__.py");
+        .spawn()
+        .expect("Creating a git repository Failed.");
+    let mut file =
+        File::create(format!("{}/README.md", name)).expect("Could not create __main__.py");
     let boilerplate = format!("# {}", name);
-    file.write_all(boilerplate.as_bytes()).expect("Failed to write to __main__.py");
+    file.write_all(boilerplate.as_bytes())
+        .expect("Failed to write to __main__.py");
 }
 
 fn create_bin_project(name: &str) {
     create_basic_dir(name);
-    let mut file = File::create(format!("{}/src/__main__.py", name)).expect("Could not create __main__.py");
+    let mut file =
+        File::create(format!("{}/src/__main__.py", name)).expect("Could not create __main__.py");
     let boilerplate =
         "def main():\n    print(\"Hello World\")\n\nif __name__ == \"__main__\":\n    main()"
-        .to_string();
-    file.write_all(boilerplate.as_bytes()).expect("Failed to write to __main__.py");
+            .to_string();
+    file.write_all(boilerplate.as_bytes())
+        .expect("Failed to write to __main__.py");
 }
 
 fn create_lib_project(name: &str) {
     create_basic_dir(name);
-    let _ = File::create(format!("{}/src/__init__.py", name)).expect("Could not create __main__.py");
+    let _ =
+        File::create(format!("{}/src/__init__.py", name)).expect("Could not create __main__.py");
 }
 
 fn cargs() -> Option<ProjectType> {
@@ -60,26 +66,35 @@ fn cargs() -> Option<ProjectType> {
         .version(crate_version!())
         .author(AUTHOR)
         .about(crate_description!())
-        .subcommand(SubCommand::with_name("new")
-            .about("Used for configuration")
-            .arg(Arg::with_name("project_name")
-                .help("Set name of project")
-                .takes_value(true)
-                .index(1))
-            .arg(Arg::with_name("binary")
-                    .long("bin")
-                    .help("Makes a file structure with a __main__.py in src folder.")
-                    .takes_value(false),
-            )
-            .arg(Arg::with_name("library")
-                    .long("lib")
-                    .help("Makes a library file structure with a __init__.py in src folder.")
-                    .takes_value(false),
-            ))
+        .subcommand(
+            SubCommand::with_name("new")
+                .about("Used for configuration")
+                .arg(
+                    Arg::with_name("project_name")
+                        .help("Set name of project")
+                        .takes_value(true)
+                        .index(1),
+                )
+                .arg(
+                    Arg::with_name("binary")
+                        .long("bin")
+                        .help("Makes a file structure with a __main__.py in src folder.")
+                        .takes_value(false),
+                )
+                .arg(
+                    Arg::with_name("library")
+                        .long("lib")
+                        .help("Makes a library file structure with a __init__.py in src folder.")
+                        .takes_value(false),
+                ),
+        )
         .get_matches();
 
     if let Some(matches) = matches.subcommand_matches("new") {
-        let project_name = matches.value_of("project_name").unwrap_or("default").to_string();
+        let project_name = matches
+            .value_of("project_name")
+            .unwrap_or("default")
+            .to_string();
         if matches.is_present("library") {
             return Some(ProjectType::Lib(project_name));
         }
